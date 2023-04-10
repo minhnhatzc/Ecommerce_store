3e74f328dc62f9e0581e166a52678c96d505abe6
Pham Van Nhat <114339708+minhnhatzc@users.noreply.github.com>
2023-04-10 19:04:58 +0700

    frontend

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9a1d71c3436fc89d"; }
+/******/ 		__webpack_require__.h = function() { return "abc545e8c18dfea2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
       if (window.scrollY > lastScrollY && !mobileMenu) {
         setShow("-translate-y-[80px]");
       } else {
-        setShow("shadow-sm")
+        setShow("shadow-sm");
       }
     } else {
       setShow("translate-y-0");
@@ -62,17 +62,19 @@ function Header() {
             </div>
           </div>
 
-          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-            <BsCart className="text-[15px] md:text-[20px]" />
-            <div
-              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px]
+          <Link href="/cart">
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+              <BsCart className="text-[15px] md:text-[20px]" />
+              <div
+                className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px]
             rounded-full bg-red-600 absolute top-1 left-5 md:left-7
             text-white text-[10px] md:text-[12px]
             flex justify-center items-center px-[2px] md:px-[5px]"
-            >
-              5
+              >
+                5
+              </div>
             </div>
-          </div>
+          </Link>
           <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
             {mobileMenu ? (
               <VscChromeClose
